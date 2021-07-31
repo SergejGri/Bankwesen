c0c4445ff47b0f7b643a249addf84ac1e5320245
Sergej <email.sergej@web.de>
2021-07-31 17:23:23 +0200

    created db class

--- Reader.py
+++ Reader.py
@@ -1,5 +1,5 @@
 import os
-import numpy as np
+import sqlite3
 from datetime import date
 from datetime import datetime
 from fpdf import FPDF
@@ -7,21 +7,81 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+class DB():
+    def __init__(self):
+        self.conn = sqlite3.connect('credit.db')
+        self.c = self.conn.cursor()
+        if not os.path.isfile('credit.db'):
+            self.c.execute("""CREATE TABLE credit (
+                    date TEXT,
+                    value REAL
+                )""")
+            self.conn.commit()
+            self.conn.close()
+
+
+
+
 class Reader:
-    def __init__(self, path, path_fin):
+    def __init__(self, path, path_fin, mode=None):
         self.path = path
         self.path_fin = path_fin
         self.df = None
-        self.get_files()
-        self.plot()
+        self.mode = mode
+        if self.mode is None:
+            self.mode = 'MT940'
+        self.file = None
+        self.dates = []
+        self.values = []
 
-    def get_files(self):
-        for p in os.listdir(self.path):
-            if os.path.isfile(p) and p.endswith('.csv') or p.endswith('.CSV'):
-                self.df = pd.read_csv(p, sep=";", encoding='cp1252')
-                print(p)
-                self.df['Buchungstag_YYYY'] = self.df['Buchungstag'].apply(lambda a: self._year_trafo(a, trafo=True))
-                self.df['Valutadatum_YYYY'] = self.df['Valutadatum'].apply(lambda a: self._year_trafo(a, trafo=True))
+        self.check_files()
+
+
+    def check_files(self):
+        if self.mode == 'MT940':
+            for p in os.listdir(self.path):
+                if os.path.isfile(p) and p.endswith('.txt') or p.endswith('.TXT'):
+                    with open(p, 'r') as f:
+                        for line in f:
+                            if ':20:STARTUMSE\n' in line:
+                                f.close()
+                                self.file = p
+                                self.parse_line()
+                                break
+        else:
+            for p in os.listdir(self.path):
+                if os.path.isfile(p) and p.endswith('.csv') or p.endswith('.CSV'):
+                    self.df = pd.read_csv(p, sep=";", encoding='cp1252')
+                    print(p)
+                    self.df['Buchungstag_YYYY'] = self.df['Buchungstag'].apply(lambda a: self._year_trafo(a, trafo=True))
+                    #self.df['Valutadatum_YYYY'] = self.df['Valutadatum'].apply(lambda a: self._year_trafo(a, trafo=True))
+
+
+    def feldnummern(self):
+        Auftragsreferenz_Nr = ':20:'
+        Bezugsreferenz = ':21:'
+        Kontobezeichnung = ':25:'
+        saldo_x = ':60x:'
+        saldo_f = ':60F:'
+
+
+    def parse_line(self):
+        with open(self.file, 'r') as f:
+            for line in f:
+                if ':60F:' in line:
+                    date_T= self._year_trafo(line.split('EUR')[0], trafo=True)
+                    credit = line.split('EUR')[1]
+                    credit = credit.replace(',', '.')
+                    credit = float(credit)
+
+
+                    self.dates.append(date_T)
+                    self.values.append(credit)
+
+
+    def field_number_MT940(self):
+        pass
+
 
     def create_dash(self):
         pass
@@ -31,12 +91,14 @@ class Reader:
 
     def plot(self):
         delta = self._get_delta_t()
-        self.df = self.df.iloc[:, ::-1]
-        x = self.df['Buchungstag_YYYY'].columns
+        #self.df = self.df.iloc[:, ::-1]
+        x = self.df['Buchungstag_YYYY']
         y = self.df['Betrag']
+
 
         fig, ax = plt.subplots(nrows=1, ncols=1)
         ax.plot(x, y)
+        ax.set_xticklabels(x[::3], rotation=45)
         ax.set_xticks(x[::10])
         ax.set_title('Test')
         ax.set_xlabel('Date')
@@ -51,19 +113,21 @@ class Reader:
         delta = d1 - d0
         return delta
 
-    @staticmethod
-    def _year_trafo(expr, trafo=False):
-        if np.isnan(expr):
-            pass
+    def _year_trafo(self, expr, trafo=False):
+        if self.mode == 'MT940':
+            expr = expr[-6:]
+            day_ = expr[-2:]
+            month_ = expr[2:4]
+            year_ = expr[:2]
         else:
             year_ = expr.split('.')[2]
             month_ = expr.split('.')[1]
             day_ = expr.split('.')[0]
-            if trafo:
-                date_ = day_ + '.' + month_ + '.' + str(20) + year_
-                return date_
-            else:
-                return year_, month_, day_
+        if trafo:
+            date_ = day_ + '.' + month_ + '.' + str(20) + year_
+            return date_
+        else:
+            return year_, month_, day_
 
 
 class PDFCreator:
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import sqlite3
 import Reader as rd
 
 
+
+
+def extend_db():
+    pass
+
 def main():
     path_ = r'C:\Users\Rechenfuchs\Desktop\Projekte\Bankwesen'
-    path_fin = r'C:\Users\Rechenfuchs\Desktop\Projekte\Bankwesen\processed_files'
-    csv_reader = rd.Reader(path_,path_fin)
+    path_fin_ = r'C:\Users\Rechenfuchs\Desktop\Projekte\Bankwesen\processed_files'
 
-    csv_reader.plot()
+    db_reader = rd.DB()
+    csv_reader = rd.Reader(path_, path_fin_, mode='MT940')
 
+
+    extend_db()
 
 if __name__ == "__main__":
     main()
